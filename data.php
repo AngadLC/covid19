04f458a14e8ec30b21092c1f8e2a03881420d71e
Angad Lamichhane <71200732+AngadLC@users.noreply.github.com>
2020-12-19 17:17:52 +0545

    database change

--- data.php
+++ data.php
@@ -1,6 +1,6 @@
 <?php
     include "db.php";
-    $get_data = "SELECT name, condition, ST_AsGeoJSON(geom) FROM enteries WHERE condition = 'covid' ";
+    $get_data = "SELECT name, condition, ST_AsGeoJSON(geom) FROM enteries";
     $result = pg_fetch_all(pg_query($dbconn, $get_data));
     echo json_encode($result)
     
--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $server = 'localhost';
 $username = 'postgres';
 $password = 'angad';
-$db_name = 'covid_range';
+$db_name = 'covid19';
 $dbconn = pg_connect("host=$server port=5432 dbname=$db_name user=$username password=$password")
 
 ?>
